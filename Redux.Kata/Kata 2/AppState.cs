ed8ec366030a0b2aee0fcb0f317a04bc60ea0b00
Alexandre Teixeira <ateixeira@askmethat.fr>
2025-04-22 19:53:56 +0200

    mob next [ci-skip] [ci skip] [skip ci]

--- Redux.Kata/Kata 2/AppState.cs
+++ Redux.Kata/Kata 2/AppState.cs
@@ -46,30 +46,3 @@ internal record AppState
         return new AppState();
     }
 }
-
-public record Store
-{
-    private Store(ISlice[] slices)
-    {
-        foreach (var slice in slices)
-        {
-            _state = _state.AddSlice(slice);
-        }
-    }
-
-    public static Store Init(params ISlice[] slices)
-    {
-        return new Store(slices);
-    }
-
-
-    private readonly AppState _state = AppState.InitialState();
-
-    public TSlice? GetSlice<TSlice>() where TSlice : class, ISlice =>
-        _state.GetSlice<TSlice>();
-
-    public TSlice UpdateSlice<TSlice>(TSlice update) where TSlice : class, ISlice
-    {
-        return _state.UpdateSlice(update);
-    }
-}
---
+++ Redux.Kata/Kata 2/Store.cs
@@ -0,0 +1,28 @@
+namespace Redux.Kata.Kata_2;
+
+public record Store
+{
+    private Store(ISlice[] slices)
+    {
+        foreach (var slice in slices)
+        {
+            _state = _state.AddSlice(slice);
+        }
+    }
+
+    public static Store Init(params ISlice[] slices)
+    {
+        return new Store(slices);
+    }
+
+
+    private readonly AppState _state = AppState.InitialState();
+
+    public TSlice? GetSlice<TSlice>() where TSlice : class, ISlice =>
+        _state.GetSlice<TSlice>();
+
+    public TSlice UpdateSlice<TSlice>(TSlice update) where TSlice : class, ISlice
+    {
+        return _state.UpdateSlice(update);
+    }
+}
